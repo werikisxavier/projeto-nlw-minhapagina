e5e18118e7f4dc25198afa7d70148c9ba12e0c98
= <werikis90@hotmail.com>
2021-10-27 16:58:21 -0300

    linkedln add

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 
-    
+ 
     
     const linksSocialMedia = {
       github: 'werikisxavier',
@@ -30,4 +30,6 @@
 
     }
 
-    getGitHubProfileInfos()
+    getGitHubProfileInfos();
+
+<script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
