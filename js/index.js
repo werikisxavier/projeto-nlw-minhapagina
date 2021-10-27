07c210f70e7dee473b47d79f2c540d7078638da8
= <werikis90@hotmail.com>
2021-10-27 17:32:25 -0300

    linkedln add

--- js/index.js
+++ js/index.js
@@ -30,6 +30,5 @@
 
     }
 
-    getGitHubProfileInfos();
+    getGitHubProfileInfos()
 
-<script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
